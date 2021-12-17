c7783cb7b3fa5d9b74b44398412a523e6e7b7de9
Ivan Grabovski <ivangrabovski@gmail.com>
2021-12-17 18:14:26 +0300

    imlemented example

--- source/observer/example.ts
+++ source/observer/example.ts
@@ -1,4 +1,18 @@
-import { IListener } from "./listener/iListener";
 import { ListenerTypeOne } from "./listener/ListenerTypeOne";
+import { ListenerTypeTwo } from "./listener/ListenerTypeTwo";
+import { PublisherOne } from "./publisher/publisherOne";
 
-let list = new ListenerTypeOne()
+let listenerTypeOne_1 = new ListenerTypeOne()
+let listenerTypeOne_2 = new ListenerTypeOne()
+let listenerTypeTwo_1 = new ListenerTypeTwo()
+
+let pulisherOne = new PublisherOne()
+
+pulisherOne.registerListener(listenerTypeOne_1)
+pulisherOne.registerListener(listenerTypeOne_2)
+pulisherOne.registerListener(listenerTypeTwo_1)
+
+
+pulisherOne.notifyAll()
+
+pulisherOne.notifyAll()
